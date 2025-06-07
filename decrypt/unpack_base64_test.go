32fcd1b9ddf8bc2ff8100248abdf671daaeaaa99
Alopex6414 <1029535012@qq.com>
2025-06-07 22:18:19 +0800

    create test functions

---
+++ decrypt/unpack_base64_test.go
@@ -0,0 +1,513 @@
+package decrypt
+
+import (
+	"bytes"
+	"io/ioutil"
+	. "qora/utils"
+	"sync"
+	"testing"
+)
+
+// TestUnpackBase64 function
+func TestUnpackBase64(t *testing.T) {
+	src := "../test/data/unpack/file_base64.txt"
+	dest := "../test/data/unpack/"
+	err := UnpackBase64(src, dest)
+	if err != nil {
+		t.Fatal("Error Unpack Base64:", err)
+	}
+}
+
+// TestUnpackBase64Confine function
+func TestUnpackBase64Confine(t *testing.T) {
+	src := "../test/data/unpack/file_base64.txt"
+	dest := "../test/data/unpack/"
+	err := UnpackBase64Confine(src, dest)
+	if err != nil {
+		t.Fatal("Error Unpack Base64:", err)
+	}
+}
+
+// TestUnpackBase64ToFile function
+func TestUnpackBase64ToFile(t *testing.T) {
+	src := "../test/data/unpack/file_base64.txt"
+	dest := "../test/data/unpack/"
+	target := "file_1.txt"
+	err := UnpackBase64ToFile(src, target, dest)
+	if err != nil {
+		t.Fatal("Error Unpack Base64 To File:", err)
+	}
+}
+
+// TestUnpackBase64ToFileConfine function
+func TestUnpackBase64ToFileConfine(t *testing.T) {
+	src := "../test/data/unpack/file_base64.txt"
+	dest := "../test/data/unpack/"
+	target := "file_1.txt"
+	err := UnpackBase64ToFileConfine(src, target, dest)
+	if err != nil {
+		t.Fatal("Error Unpack Base64 To File:", err)
+	}
+}
+
+// TestUnpackBase64ToMemory function
+func TestUnpackBase64ToMemory(t *testing.T) {
+	var dest []byte
+	src := "../test/data/unpack/file_base64.txt"
+	target := "file_1.txt"
+	err := UnpackBase64ToMemory(src, target, &dest)
+	if err != nil {
+		t.Fatal("Error Unpack Base64 To Memory:", err)
+	}
+}
+
+// TestBase64ExtractInfo function
+func TestBase64ExtractInfo(t *testing.T) {
+	var dest []string
+	var sz []int
+	src := "../test/data/unpack/file_base64.txt"
+	err := UnpackBase64ExtractInfo(src, &dest, &sz)
+	if err != nil {
+		t.Fatal("Error Unpack Base64 Extract Information:", err)
+	}
+	if len(dest) != 5 {
+		t.Fatal("Error Extract file number")
+	}
+}
+
+// TestUnpackBase64WorkCalculate function
+func TestUnpackBase64WorkCalculate(t *testing.T) {
+	src := "../test/data/unpack/file_base64.txt"
+	_, err := UnpackBase64WorkCalculate(src)
+	if err != nil {
+		t.Fatal("Error Unpack Base64 Work Calculate:", err)
+	}
+}
+
+// TestUnpackBase64OneToMemory function
+func TestUnpackBase64OneToMemory(t *testing.T) {
+	var dest string
+	src := []byte{
+		0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+		0x62, 0x47, 0x51, 0x68,
+	}
+	h := TUnpackBase64One{}
+	h.Name = make([]byte, 32)
+	h.Size = make([]byte, 4)
+	rd := bytes.NewReader(src)
+	_, err := rd.Read(h.Name)
+	if err != nil {
+		t.Fatal("Error read header name:", err)
+	}
+	_, err = rd.Read(h.Size)
+	if err != nil {
+		t.Fatal("Error read header size:", err)
+	}
+	s := make([]byte, BytesToInt(h.Size))
+	n, err := rd.Read(s)
+	if n <= 0 {
+		t.Fatal("Error read body:", err)
+	}
+	err = UnpackBase64OneToMemory(s, &dest)
+	if err != nil {
+		t.Fatal("Error unpack crypt file:", err)
+	}
+	if dest != "hello,world!" {
+		t.Fatal("Error unpack content:", dest)
+	}
+}
+
+// TestUnpackBase64OneGo function
+func TestUnpackBase64OneGo(t *testing.T) {
+	var wg sync.WaitGroup
+	src := []byte{
+		0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+		0x62, 0x47, 0x51, 0x68,
+	}
+	dest := "../test/data/unpack/"
+	h := TUnpackBase64One{}
+	h.Name = make([]byte, 32)
+	h.Size = make([]byte, 4)
+	rd := bytes.NewReader(src)
+	_, err := rd.Read(h.Name)
+	if err != nil {
+		t.Fatal("Error read header name:", err)
+	}
+	_, err = rd.Read(h.Size)
+	if err != nil {
+		t.Fatal("Error read header size:", err)
+	}
+	s := make([]byte, BytesToInt(h.Size))
+	n, err := rd.Read(s)
+	if n <= 0 {
+		t.Fatal("Error read body:", err)
+	}
+	wg.Add(1)
+	go UnpackBase64OneGo(s, h, dest, &wg)
+	wg.Wait()
+}
+
+// TestUnpackBase64One function
+func TestUnpackBase64One(t *testing.T) {
+	src := []byte{
+		0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+		0x62, 0x47, 0x51, 0x68,
+	}
+	dest := "../test/data/unpack/"
+	h := TUnpackBase64One{}
+	h.Name = make([]byte, 32)
+	h.Size = make([]byte, 4)
+	rd := bytes.NewReader(src)
+	_, err := rd.Read(h.Name)
+	if err != nil {
+		t.Fatal("Error read header name:", err)
+	}
+	_, err = rd.Read(h.Size)
+	if err != nil {
+		t.Fatal("Error read header size:", err)
+	}
+	s := make([]byte, BytesToInt(h.Size))
+	n, err := rd.Read(s)
+	if n <= 0 {
+		t.Fatal("Error read body:", err)
+	}
+	err = UnpackBase64One(s, h, dest)
+	if err != nil {
+		t.Fatal("Error unpack crypt file:", err)
+	}
+}
+
+// TestUnpackBase64OneConfine function
+func TestUnpackBase64OneConfine(t *testing.T) {
+	src := []byte{
+		0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+		0x62, 0x47, 0x51, 0x68,
+	}
+	dest := "../test/data/unpack/"
+	h := TUnpackBase64One{}
+	h.Name = make([]byte, 32)
+	h.Size = make([]byte, 4)
+	rd := bytes.NewReader(src)
+	_, err := rd.Read(h.Name)
+	if err != nil {
+		t.Fatal("Error read header name:", err)
+	}
+	_, err = rd.Read(h.Size)
+	if err != nil {
+		t.Fatal("Error read header size:", err)
+	}
+	s := make([]byte, BytesToInt(h.Size))
+	n, err := rd.Read(s)
+	if n <= 0 {
+		t.Fatal("Error read body:", err)
+	}
+	err = UnpackBase64OneConfine(s, h, dest)
+	if err != nil {
+		t.Fatal("Error unpack crypt file:", err)
+	}
+}
+
+// TestBase64DecryptGo function
+func TestBase64DecryptGo(t *testing.T) {
+	var wg sync.WaitGroup
+	var r string
+	src := "aGVsbG8sd29ybGQh"
+	dest := "hello,world!"
+	wg.Add(1)
+	go Base64DecryptGo(src, &r, &wg)
+	wg.Wait()
+	if r != dest {
+		t.Errorf("Error Decrypt Base64.")
+	}
+	err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write Base64 One:", err)
+	}
+}
+
+// TestBase64Decrypt function
+func TestBase64Decrypt(t *testing.T) {
+	src := "aGVsbG8sd29ybGQh"
+	dest := "hello,world!"
+	r := Base64Decrypt(src)
+	if r != dest {
+		t.Errorf("Error Decrypt Base64.")
+	}
+	err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write Base64 One:", err)
+	}
+}
+
+// BenchmarkUnpackBase64 function
+func BenchmarkUnpackBase64(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_base64.txt"
+		dest := "../test/data/unpack/"
+		err := UnpackBase64(src, dest)
+		if err != nil {
+			b.Fatal("Error Unpack Base64:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64Confine function
+func BenchmarkUnpackBase64Confine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_base64.txt"
+		dest := "../test/data/unpack/"
+		err := UnpackBase64Confine(src, dest)
+		if err != nil {
+			b.Fatal("Error Unpack Base64:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64ToFile function
+func BenchmarkUnpackBase64ToFile(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_base64.txt"
+		dest := "../test/data/unpack/"
+		target := "file_1.txt"
+		err := UnpackBase64ToFile(src, target, dest)
+		if err != nil {
+			b.Fatal("Error Unpack Base64 To File:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64ToFileConfine function
+func BenchmarkUnpackBase64ToFileConfine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_base64.txt"
+		dest := "../test/data/unpack/"
+		target := "file_1.txt"
+		err := UnpackBase64ToFileConfine(src, target, dest)
+		if err != nil {
+			b.Fatal("Error Unpack Base64 To File:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64ToMemory function
+func BenchmarkUnpackBase64ToMemory(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var dest []byte
+		src := "../test/data/unpack/file_base64.txt"
+		target := "file_1.txt"
+		err := UnpackBase64ToMemory(src, target, &dest)
+		if err != nil {
+			b.Fatal("Error Unpack Base64 To Memory:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64ExtractInfo function
+func BenchmarkUnpackBase64ExtractInfo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var dest []string
+		var sz []int
+		src := "../test/data/unpack/file_base64.txt"
+		err := UnpackBase64ExtractInfo(src, &dest, &sz)
+		if err != nil {
+			b.Fatal("Error Unpack Base64 Extract Information:", err)
+		}
+		if len(dest) != 5 {
+			b.Fatal("Error Extract file number")
+		}
+	}
+}
+
+// BenchmarkUnpackBase64WorkCalculate function
+func BenchmarkUnpackBase64WorkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_base64.txt"
+		_, err := UnpackBase64WorkCalculate(src)
+		if err != nil {
+			b.Fatal("Error Unpack Base64 Work Calculate:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64OneToMemory function
+func BenchmarkUnpackBase64OneToMemory(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var dest string
+		src := []byte{
+			0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+			0x62, 0x47, 0x51, 0x68,
+		}
+		h := TUnpackBase64One{}
+		h.Name = make([]byte, 32)
+		h.Size = make([]byte, 4)
+		rd := bytes.NewReader(src)
+		_, err := rd.Read(h.Name)
+		if err != nil {
+			b.Fatal("Error read header name:", err)
+		}
+		_, err = rd.Read(h.Size)
+		if err != nil {
+			b.Fatal("Error read header size:", err)
+		}
+		s := make([]byte, BytesToInt(h.Size))
+		n, err := rd.Read(s)
+		if n <= 0 {
+			b.Fatal("Error read body:", err)
+		}
+		err = UnpackBase64OneToMemory(s, &dest)
+		if err != nil {
+			b.Fatal("Error unpack crypt file:", err)
+		}
+		if dest != "hello,world!" {
+			b.Fatal("Error unpack content:", dest)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64OneGo function
+func BenchmarkUnpackBase64OneGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		src := []byte{
+			0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+			0x62, 0x47, 0x51, 0x68,
+		}
+		dest := "../test/data/unpack/"
+		h := TUnpackBase64One{}
+		h.Name = make([]byte, 32)
+		h.Size = make([]byte, 4)
+		rd := bytes.NewReader(src)
+		_, err := rd.Read(h.Name)
+		if err != nil {
+			b.Fatal("Error read header name:", err)
+		}
+		_, err = rd.Read(h.Size)
+		if err != nil {
+			b.Fatal("Error read header size:", err)
+		}
+		s := make([]byte, BytesToInt(h.Size))
+		n, err := rd.Read(s)
+		if n <= 0 {
+			b.Fatal("Error read body:", err)
+		}
+		wg.Add(1)
+		go UnpackBase64OneGo(s, h, dest, &wg)
+		wg.Wait()
+	}
+}
+
+// BenchmarkUnpackBase64One function
+func BenchmarkUnpackBase64One(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte{
+			0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+			0x62, 0x47, 0x51, 0x68,
+		}
+		dest := "../test/data/unpack/"
+		h := TUnpackBase64One{}
+		h.Name = make([]byte, 32)
+		h.Size = make([]byte, 4)
+		rd := bytes.NewReader(src)
+		_, err := rd.Read(h.Name)
+		if err != nil {
+			b.Fatal("Error read header name:", err)
+		}
+		_, err = rd.Read(h.Size)
+		if err != nil {
+			b.Fatal("Error read header size:", err)
+		}
+		s := make([]byte, BytesToInt(h.Size))
+		n, err := rd.Read(s)
+		if n <= 0 {
+			b.Fatal("Error read body:", err)
+		}
+		err = UnpackBase64One(s, h, dest)
+		if err != nil {
+			b.Fatal("Error unpack crypt file:", err)
+		}
+	}
+}
+
+// BenchmarkUnpackBase64One function
+func BenchmarkUnpackBase64OneConfine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte{
+			0x66, 0x69, 0x6C, 0x65, 0x2E, 0x74, 0x78, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x10, 0x61, 0x47, 0x56, 0x73, 0x62, 0x47, 0x38, 0x73, 0x64, 0x32, 0x39, 0x79,
+			0x62, 0x47, 0x51, 0x68,
+		}
+		dest := "../test/data/unpack/"
+		h := TUnpackBase64One{}
+		h.Name = make([]byte, 32)
+		h.Size = make([]byte, 4)
+		rd := bytes.NewReader(src)
+		_, err := rd.Read(h.Name)
+		if err != nil {
+			b.Fatal("Error read header name:", err)
+		}
+		_, err = rd.Read(h.Size)
+		if err != nil {
+			b.Fatal("Error read header size:", err)
+		}
+		s := make([]byte, BytesToInt(h.Size))
+		n, err := rd.Read(s)
+		if n <= 0 {
+			b.Fatal("Error read body:", err)
+		}
+		err = UnpackBase64OneConfine(s, h, dest)
+		if err != nil {
+			b.Fatal("Error unpack crypt file:", err)
+		}
+	}
+}
+
+// BenchmarkBase64DecryptGo function
+func BenchmarkBase64DecryptGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		var r string
+		src := "aGVsbG8sd29ybGQh"
+		dest := "hello,world!"
+		wg.Add(1)
+		go Base64DecryptGo(src, &r, &wg)
+		wg.Wait()
+		if r != dest {
+			b.Errorf("Error Decrypt Base64.")
+		}
+		err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write Base64 One:", err)
+		}
+	}
+}
+
+// BenchmarkBase64Decrypt function
+func BenchmarkBase64Decrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "aGVsbG8sd29ybGQh"
+		dest := "hello,world!"
+		r := Base64Decrypt(src)
+		if r != dest {
+			b.Errorf("Error Decrypt Base64.")
+		}
+		err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write Base64 One:", err)
+		}
+	}
+}
---
+++ encrypt/pack_sha1_test.go
@@ -0,0 +1,126 @@
+package encrypt
+
+import (
+	"crypto/sha1"
+	"io/ioutil"
+	"sync"
+	"testing"
+)
+
+// TestSHA1Check function
+func TestSHA1Check(t *testing.T) {
+	src := "hello,world!"
+	dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+	r := SHA1Check(src, dest)
+	if !r {
+		t.Fatal("Error Check SHA1:")
+	}
+}
+
+// TestSHA1Check2 function
+func TestSHA1Check2(t *testing.T) {
+	src := "Nice to meet you~"
+	dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+	r := SHA1Check(src, dest)
+	if r {
+		t.Fatal("Error Check SHA1:")
+	}
+}
+
+// TestSHA1Encode function
+func TestSHA1Encode(t *testing.T) {
+	src := "hello,world!"
+	r := SHA1Encode(src)
+	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA1 Encode:", err)
+	}
+}
+
+// TestSHA1EncryptGo function
+func TestSHA1EncryptGo(t *testing.T) {
+	var wg sync.WaitGroup
+	var r [sha1.Size]byte
+	src := []byte("hello,world!")
+	wg.Add(1)
+	go SHA1EncryptGo(src, &r, &wg)
+	wg.Wait()
+	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:sha1.Size], 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA1 Encrypt:", err)
+	}
+}
+
+// TestSHA1Encrypt function
+func TestSHA1Encrypt(t *testing.T) {
+	src := []byte("hello,world!")
+	r := SHA1Encrypt(src)
+	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:sha1.Size], 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA1 Encrypt:", err)
+	}
+}
+
+// BenchmarkSHA1Check function
+func BenchmarkSHA1Check(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+		r := SHA1Check(src, dest)
+		if !r {
+			b.Fatal("Error Check SHA1:")
+		}
+	}
+}
+
+// BenchmarkSHA1Check2 function
+func BenchmarkSHA1Check2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "Nice to meet you~"
+		dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+		r := SHA1Check(src, dest)
+		if r {
+			b.Fatal("Error Check SHA1:")
+		}
+	}
+}
+
+// BenchmarkSHA1Encode function
+func BenchmarkSHA1Encode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		r := SHA1Encode(src)
+		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA1 Encode:", err)
+		}
+	}
+}
+
+// BenchmarkSHA1EncryptGo function
+func BenchmarkSHA1EncryptGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		var r [sha1.Size]byte
+		src := []byte("hello,world!")
+		wg.Add(1)
+		go SHA1EncryptGo(src, &r, &wg)
+		wg.Wait()
+		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:sha1.Size], 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA1 Encrypt:", err)
+		}
+	}
+}
+
+// BenchmarkSHA1Encrypt function
+func BenchmarkSHA1Encrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte("hello,world!")
+		r := SHA1Encrypt(src)
+		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:sha1.Size], 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA1 Encrypt:", err)
+		}
+	}
+}
