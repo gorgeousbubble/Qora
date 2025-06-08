daf554829a8e9ebe7098d4ffed816c53156ddb88
Alopex6414 <1029535012@qq.com>
2025-06-08 17:58:37 +0800

    update fmt.bat & test.bat

---
+++ fmt.bat
@@ -0,0 +1,15 @@
+:: Qora Project - Format(Golang)
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Start Format All
+echo Start Format Qora:
+goto fmt_all
+
+:: Traversing folder format Golang code
+:fmt_all
+echo fmt all:
+for /d %%i in (*) do echo %%i & cd %%i & echo. & if not %%i == .github ( if not %%i == .idea ( if not %%i == .vscode ( if not %%i == 3pp ( if not %%i == bin ( if not %%i == doc ( if not %%i == log ( if not %%i == plugin ( if not %%i == test ( go fmt ) ) ) ) ) ) ) ) ) & echo. & echo. & cd ..
+
+:: Pause format
+pause
---
+++ test.bat
@@ -0,0 +1,15 @@
+:: Qora Project - Test(Golang)
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Start Test All
+echo Start Test Qora:
+goto test_all
+
+:: Traversing folder test Golang code
+:test_all
+echo fmt all:
+for /d %%i in (*) do echo %%i & cd %%i & echo. & if not %%i == .github ( if not %%i == .idea ( if not %%i == .vscode ( if not %%i == 3pp ( if not %%i == bin ( if not %%i == doc ( if not %%i == log ( if not %%i == plugin ( if not %%i == test ( go test -v -cover -benchmem -bench . ) ) ) ) ) ) ) ) ) & echo. & echo. & cd ..
+
+:: Pause format
+pause
