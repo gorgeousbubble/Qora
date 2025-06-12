cec4577c45e49c4326fc9f780f6145366362599b
Alopex6414 <1029535012@qq.com>
2025-06-12 21:16:34 +0800

    create oneway-tls-service.bat

---
+++ oneway-tls-service.bat
@@ -0,0 +1,7 @@
+:: Qora Project - HTTPs Service
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Qora HTTPs Service - Start
+cd bin
+./qora https -ip localhost -port 8443
