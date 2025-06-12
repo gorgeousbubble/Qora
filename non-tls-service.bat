1c70aed7826cc447d657598d8068850f71246db8
Alopex6414 <1029535012@qq.com>
2025-06-12 21:13:57 +0800

    create non-tls-service.bat

---
+++ non-tls-service.bat
@@ -0,0 +1,7 @@
+:: Qora Project - HTTP Service
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Qora HTTP Service - Start
+cd bin
+./qora http -ip localhost -port 8080
